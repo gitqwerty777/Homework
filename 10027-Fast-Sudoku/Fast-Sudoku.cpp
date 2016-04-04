d0f9e4204db041e32afc0b64cbd5cbe1b5534d6e
gitqwerty777 <goohcl777@gmail.com>
2016-04-04 20:28:46 +0800

    now can get nextn quick

--- 10027-Fast-Sudoku/Fast-Sudoku.cpp
+++ 10027-Fast-Sudoku/Fast-Sudoku.cpp
@@ -1,6 +1,9 @@
 #include <cstdio>
 #include <omp.h>
 
+
+int nextn[9][9];
+
 inline void getPossibleValue(int sudoku[9][9], bool isLegal[], int row, int col, int blockRow, int blockCol){
   for(int i = 1; i <= 9; i ++)
     isLegal[i] = true;
@@ -11,6 +14,14 @@ inline void getPossibleValue(int sudoku[9][9], bool isLegal[], int row, int col,
   for (int i = 0; i < 3; i ++)
     for (int j = 0; j < 3; j ++)
       isLegal[sudoku[3*blockRow+i][3*blockCol+j]] = false;
+}
+
+inline int getNextIndex(int n, int sudoku[9][9]){
+  n = n+1;
+  while((n < 81) && (sudoku[n/9][n%9] != 0)){
+    n++;
+  }
+  return n;
 }
 
 int placeNumber (int n, int sudoku [9][9]){
@@ -30,7 +41,7 @@ int placeNumber (int n, int sudoku [9][9]){
     if(!isLegal[tryi])
       continue;
     sudoku[row][col] = tryi;
-    numSolution += placeNumber (n + 1, sudoku);
+    numSolution += placeNumber (nextn[row][col], sudoku);
   } /* for */
   sudoku [row][col] = 0;
   return numSolution;
@@ -39,18 +50,32 @@ int placeNumber (int n, int sudoku [9][9]){
 int main ( void ){
   int sudoku [9][9];
   int firstZero = -1;
+  int preZero = -1;
   for (int i = 0; i < 9; i++)
     for (int j = 0; j < 9; j++) {
       scanf ("%d", &( sudoku [i][j ]));
-      if ( sudoku [i][j] == 0 && firstZero == -1)
-	firstZero = i * 9 + j;
+      if ( sudoku [i][j] == 0)
+	if(firstZero == -1){
+	  firstZero = i * 9 + j;
+	  preZero = firstZero;
+	} else {
+	  nextn[preZero/9][preZero%9] = i * 9 + j;
+	  preZero = i * 9 + j;
+	}
     }
+  if(preZero != -1)
+    nextn[preZero/9][preZero%9] = 81;
   omp_set_num_threads (9);
   int numSolution = 0;
-# pragma omp parallel for reduction (+ : numSolution ) firstprivate ( sudoku ) 
+  bool isLegal[10];
+  getPossibleValue(sudoku, isLegal, firstZero/9, firstZero%9, (firstZero/9)/3, (firstZero%9)/3);
+  
+# pragma omp parallel for reduction (+ : numSolution ) firstprivate ( sudoku )
   for (int i = 1; i <= 9; i++) {
-    sudoku [ firstZero / 9][ firstZero % 9] = i;
-    numSolution += placeNumber ( firstZero , sudoku );
+    if(isLegal[i]){
+      sudoku [ firstZero / 9][ firstZero % 9] = i;
+      numSolution += placeNumber ( nextn[firstZero/9][firstZero%9] , sudoku );
+    }
   }
   printf ("%d\n", numSolution );
   return 0;
